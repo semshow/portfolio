f23ce46052da22a4f0d45ad47eac715470e0e8bb
Alex Dermenji <allexdermenji@gmail.com>
2019-07-15 17:19:35 +0100

    mark up for admin panel done

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -195,7 +195,272 @@
                   .widget__btns
                     button.widget__btns-link.widget__btns-link-edit Править
                     button.widget__btns-link.widget__btns-link-cancel Удалить
+    .admin-main
+        header.admin__header
+          .admin__container.admin__container--header
+            .admin__header-description
+              .admin__logo
+                img(src="../images/content/kovalchuk.jpg").admin__logo-pic
+              .admin__header-name Александр Дерменжи
+              .admin__header-panelname Панель администрирования
+            .admin__header-button
+              a.admin__header-link Выйти   
+        .admin__menu
+          .admin__container
+            nav.main-menu
+              ul.main-menu__list
+                li.main-menu__item
+                  a.main-menu__link Обо мне 
+                li.main-menu__item 
+                  a.main-menu__link.admin__link--active Работы 
+                li.main-menu__item 
+                  a.main-menu__link Отзывы 
+        section.admin__content
+          .admin__container
+            .admin__content-header
+              h1.admin__content-title Блок "Работы" 
+            .admin__works
+              .admin__works-widget.admin__works-widget--works
+                button.add.add--widget + 
+                p.admin__works-widget-text Добавить работу 
+              .admin__works-widget.widget 
+                .widget__img
+                  img(src="../images/content/preview1.jpg")
+                  ul.widget__skills
+                    li.edit__tag HTML
+                    li.edit__tag CSS
+                    li.edit__tag JavaScript  
+                .widget__content    
+                  .widget__title Сайт школы образования  
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
                     
+              .admin__works-widget.widget 
+                .widget__img
+                  img(src="../images/content/preview2.jpg")
+                  ul.widget__skills
+                    li.edit__tag HTML
+                    li.edit__tag CSS
+                    li.edit__tag JavaScript  
+                .widget__content    
+                  .widget__title Сайт школы образования  
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+              .admin__works-widget.widget 
+                .widget__img
+                  img(src="../images/content/preview3.jpg")
+                  ul.widget__skills
+                    li.edit__tag HTML
+                    li.edit__tag CSS
+                    li.edit__tag JavaScript  
+                .widget__content    
+                  .widget__title Сайт школы образования  
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+              .admin__works-widget.widget 
+                .widget__img
+                  img(src="../images/content/preview4.jpg")
+                  ul.widget__skills
+                    li.edit__tag HTML
+                    li.edit__tag CSS
+                    li.edit__tag JavaScript  
+                .widget__content    
+                  .widget__title Сайт школы образования  
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+    .admin-main
+        header.admin__header
+          .admin__container.admin__container--header
+            .admin__header-description
+              .admin__logo
+                img(src="../images/content/kovalchuk.jpg").admin__logo-pic
+              .admin__header-name Александр Дерменжи
+              .admin__header-panelname Панель администрирования
+            .admin__header-button
+              a.admin__header-link Выйти   
+        .admin__menu
+          .admin__container
+            nav.main-menu
+              ul.main-menu__list
+                li.main-menu__item
+                  a.main-menu__link Обо мне 
+                li.main-menu__item 
+                  a.main-menu__link Работы 
+                li.main-menu__item 
+                  a.main-menu__link.admin__link--active  Отзывы 
+        section.admin__content
+          .admin__container
+            .admin__content-header
+              h1.admin__content-title Блок "Отзывы" 
+            .block.block--addphoto
+              .block__header.block__header--works 
+                .block__header-title Новый отзыв
+              form.edit
+                .edit__addphoto
+                  .edit__addphoto-block
+                  .edit__addphoto-circle
+                  button.btn.add__photo-btn Добавить отзыв
+                .edit__addphoto-description
+                  .edit__addphoto-header
+                    label.field__title.field__title--addphoto Имя автора
+                      input(type="text" placeholder="Ковальчук Дмитрий").field.field--addphoto 
+                    label.field__title.field__title--addphoto Титул автора
+                      input(type="text" placeholder="Основатель Loftschool").field.field--addphoto   
+                  label.field__title Описание
+                  textarea.edit__textarea Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  .edit__btns
+                    button.edit__btns-cancel Отмена
+                    button.add.add--load Сохранить
+            .admin__works
+              .admin__works-widget.admin__works-widget--addphoto
+                button.add.add--widget + 
+                p.admin__works-widget-text Добавить работу 
+              .admin__works-widget.widget 
+                .widget__content    
+                  .widget__header
+                    .widget__header-img
+                      img(src="../images/content/sabantsev.jpg").widget__header-pic
+                    .widget__header--title
+                      .widget__header-title Владимир Сабанцев 
+                      .widget__title-job Преподаватель
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+                    
+              .admin__works-widget.widget 
+                .widget__content    
+                  .widget__header
+                    .widget__header-img
+                      img(src="../images/content/sabantsev.jpg").widget__header-pic
+                    .widget__header--title
+                      .widget__header-title Владимир Сабанцев 
+                      .widget__title-job Преподаватель
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+              .admin__works-widget.widget 
+                .widget__content    
+                  .widget__header
+                    .widget__header-img
+                      img(src="../images/content/sabantsev.jpg").widget__header-pic
+                    .widget__header--title
+                      .widget__header-title Владимир Сабанцев 
+                      .widget__title-job Преподаватель
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+              .admin__works-widget.widget 
+                .widget__content    
+                  .widget__header
+                    .widget__header-img
+                      img(src="../images/content/sabantsev.jpg").widget__header-pic
+                    .widget__header--title
+                      .widget__header-title Владимир Сабанцев 
+                      .widget__title-job Преподаватель
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+    .admin-main
+        header.admin__header
+          .admin__container.admin__container--header
+            .admin__header-description
+              .admin__logo
+                img(src="../images/content/kovalchuk.jpg").admin__logo-pic
+              .admin__header-name Александр Дерменжи
+              .admin__header-panelname Панель администрирования
+            .admin__header-button
+              a.admin__header-link Выйти   
+        .admin__menu
+          .admin__container
+            nav.main-menu
+              ul.main-menu__list
+                li.main-menu__item
+                  a.main-menu__link Обо мне 
+                li.main-menu__item 
+                  a.main-menu__link Работы 
+                li.main-menu__item 
+                  a.main-menu__link.admin__link--active  Отзывы 
+        section.admin__content
+          .admin__container
+            .admin__content-header
+              h1.admin__content-title Блок "Отзывы" 
+            .admin__works
+              .admin__works-widget.admin__works-widget--addphoto
+                button.add.add--widget + 
+                p.admin__works-widget-text Добавить работу 
+              .admin__works-widget.widget 
+                .widget__content    
+                  .widget__header
+                    .widget__header-img
+                      img(src="../images/content/sabantsev.jpg").widget__header-pic
+                    .widget__header--title
+                      .widget__header-title Владимир Сабанцев 
+                      .widget__title-job Преподаватель
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+                    
+              .admin__works-widget.widget 
+                .widget__content    
+                  .widget__header
+                    .widget__header-img
+                      img(src="../images/content/sabantsev.jpg").widget__header-pic
+                    .widget__header--title
+                      .widget__header-title Владимир Сабанцев 
+                      .widget__title-job Преподаватель
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+              .admin__works-widget.widget 
+                .widget__content    
+                  .widget__header
+                    .widget__header-img
+                      img(src="../images/content/sabantsev.jpg").widget__header-pic
+                    .widget__header--title
+                      .widget__header-title Владимир Сабанцев 
+                      .widget__title-job Преподаватель
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
+              .admin__works-widget.widget 
+                .widget__content    
+                  .widget__header
+                    .widget__header-img
+                      img(src="../images/content/sabantsev.jpg").widget__header-pic
+                    .widget__header--title
+                      .widget__header-title Владимир Сабанцев 
+                      .widget__title-job Преподаватель
+                  .widget__text Этот парень проходил обучение веб-разработке не где-то, а в LoftSchool! 4,5 месяца только самых тяжелых испытаний и бессонных ночей!
+                  a.widget__link http://loftschool.com
+                  .widget__btns
+                    button.widget__btns-link.widget__btns-link-edit Править
+                    button.widget__btns-link.widget__btns-link-cancel Удалить
 </template>
 <script>
 export default {}
@@ -358,6 +623,12 @@ export default {}
   padding-top: 0;
 }
 
+.block--addphoto {
+  width: 100%;
+  padding-top: 0;
+  padding-bottom: 50px;
+}
+
 .block__header {
   min-height: 58px;
   border-bottom: 1px solid rgba(fffff, 0.3);
@@ -414,6 +685,16 @@ export default {}
   background-color: transparent;
   border: 0;
   border-bottom: 1px solid black;
+}
+
+.field--addphoto {
+  display: block;
+  padding-left: 0;
+  width: 100%;
+}
+
+.field__title--addphoto {
+  width: 48%;
 }
 
 .field--edit {
@@ -585,7 +866,6 @@ export default {}
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-height: 560px;
   background: linear-gradient(to right, #006aed, #3f35cb);
   margin-bottom: 30px;
   margin-right: 2%;
@@ -604,10 +884,20 @@ export default {}
   margin-top: 30px;
 }
 
+.admin__works-widget--works {
+  min-height: 560px;
+}
+
+.admin__works-widget--addphoto {
+  min-height: 380px;
+}
+
 .widget {
   display: flex;
   justify-content: flex-start;
   background: $white;
+  box-shadow: 4px 3px 20px 0px rgba(0, 0, 0, 0.1);
+  padding-bottom: 30px;
 }
 
 .widget__img {
@@ -616,7 +906,7 @@ export default {}
   margin-bottom: 40px;
 }
 .widget__content {
-  width: 90%;
+  width: 80%;
 }
 
 .widget__skills {
@@ -687,6 +977,88 @@ export default {}
   width=100%,
   height=100%
   )
+}
+
+.edit__addphoto {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 20%;
+}
+
+
+.edit__addphoto-block {
+  position: relative;
+  width: 200px;
+  height: 200px;
+  background-color: #dee4ed;
+  border-radius: 50%;
+  margin-bottom: 25px;
+}
+
+.add__photo-btn {
+  color: #383bcf;
+  font-weight: 600;
+}
+
+.edit__addphoto-block::after {
+  content: "";
+  width: 90px;
+  height: 115px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  background: svg-load(
+    "user.svg",
+    fill=$white,
+    width=100%,
+    height=100%
+  ) no-repeat center center;
+}
+
+.edit__addphoto-description {
+  width: 76%;
+}
+.edit__addphoto-header {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 40px;
+}
+
+.widget__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 112px;
+  border-bottom: 1px solid $gray;
+  margin-bottom: 25px;
+}
+
+.widget__header-title {
+  font-size: 18px;
+  color: #414c63;
+  font-weight: 700;
+}
+
+.widget__header-img {
+  width: 50px;
+  height: 50px;
+  overflow: hidden;
+  border-radius: 50%;
+}
+
+.widget__header-pic {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: top;
+}
+
+.widget__title-job {
+  color: #414c63;
+  opacity: 0.5;
+  font-weight: 600;
 }
 
 </style>
