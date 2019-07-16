6c3d92f96df6d5eb91d6f2095805ce41ac2ab11d
Alex Dermenji <allexdermenji@gmail.com>
2019-07-16 09:01:52 +0100

    form validation done

--- src/main.js
+++ src/main.js
@@ -8,3 +8,13 @@ import "./scripts/parallax";
 import "./scripts/hamburger";
 import "./scripts/works";
 import "./scripts/reviews";
+import Validate from "./scripts/contact";
+
+const form = document.querySelector('.j-form');
+
+if (form){
+  const validate = new Validate({
+    element: 'j-form'
+  }); 
+  validate.init();
+}
